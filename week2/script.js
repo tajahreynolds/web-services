25bea99a1f96b93ddb4c7c3df793c0c4c9b56d87
TaJah Reynolds <reynolt4@miamioh.edu>
2021-02-09 23:33:09 -0500

    finsh key value retrieval

--- week2/script.js
+++ week2/script.js
@@ -1,21 +1,43 @@
-$(window).on("load", onReady);
-
-// set ajax parameters
-var settings = {
-    "url": "http://ceclnx01.cec.miamioh.edu/~reynolt4/cse451-web-public-2021/week2/kv/kv.php/api/v1/info",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-    "Content-Type": "application/json"
-    },
-    "data": data,
-}
-
-// send data and process response
-$.ajax(settings).done(function (response) {
-console.log(response);
-});
-
+/* TaJah Reynolds
+    CSE451 Week2 Assignment
+    02/09/2021
+*/
 function onReady() {
-    $.ajax(settings)
+    // set ajax params
+    let settings = {
+        "url": "http://ceclnx01.cec.miamioh.edu/~reynolt4/cse451-reynolt4-web/week2/week2-rest.php/api/v1/info",
+        "method": "GET",
+        "timeout": 0,
+    };
+    // send query and process response
+    $.ajax(settings).done(function (response) {
+        console.log(response.keys);
+        for (let key in response.keys) {
+            addToTable(key);
+        }
+    });
 }
+
+function addToTable(key) {
+    // set ajax params
+    let settings = {
+        "url": "http://ceclnx01.cec.miamioh.edu/~reynolt4/cse451-reynolt4-web/week2/week2-rest.php/api/v1/info/" + key,
+        "method": "GET",
+        "timeout": 0,
+    };
+    // send query and process response
+    $.ajax(settings).done(function (response) {
+        $("#pairs").append("<tr><td>" + key + "</td><td>" + response.value + "</td></tr>");
+    }).fail(function (error) {
+        console.error(error);
+    });
+}
+
+
+
+
+
+
+
+
+$(window).on("load", onReady);
