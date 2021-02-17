1b4ed2e8b1b1b168fc23c3f78146eaf54c4ff23d
TaJah Reynolds <reynolt4@miamioh.edu>
2021-02-17 17:29:40 -0500

    Update index.php

---
+++ week3_auth/index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    Hello World!
+</body>
+</html>
