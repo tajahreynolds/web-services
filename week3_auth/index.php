b31c049e0b067d0cd346473a7943fd8019722723
TaJah Reynolds <reynolt4@miamioh.edu>
2021-02-17 20:50:55 -0500

    Update index.php

--- week3_auth/index.php
+++ week3_auth/index.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
-require_once("passwords.php");
-$mysqli = mysqli_connect($host, $user, $pass, $db);
-if (mysqli_connect_errno($mysqli)) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    die;
-}
+if (!isset($_SESSION['username'])):
+    $_SESSION['username'] = "";
 ?>
 
 <!DOCTYPE html>
@@ -17,12 +13,30 @@ if (mysqli_connect_errno($mysqli)) {
 </head>
 
 <body>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
         Username: <input type="text" name="username"><br>
         Password: <input type="password" name="password"><br>
         <input type="submit">
     </form>
 </body>
 </html>
+
+<?php
+endif;
+
+if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
+    $username = htmlspecialchars($_REQUEST['username']);
+    $password = hash('sha256',$_REQUEST['password']);
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+}
+require_once("passwords.php");
+$mysqli = mysqli_connect($host, $user, $pass, $db);
+if (mysqli_connect_errno($mysqli)) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    die;
+}
+?>
+
 
 
