c396fcd5051b209d7035d89effe3411356560071
TaJah Reynolds <reynolt4@miamioh.edu>
2021-02-17 19:37:29 -0500

    Update index.php

--- week3_auth/index.php
+++ week3_auth/index.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+require_once("passwords.php");
+$mysqli = mysqli_connect($host, $user, $pass, $db);
+if (mysqli_connect_errno($mysqli)) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    die;
+}
 ?>
 
 <!DOCTYPE html>
