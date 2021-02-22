a132a68c04a32a8deae41345a8df8289a1472e66
TaJah Reynolds <reynolt4@miamioh.edu>
2021-02-22 16:28:23 -0500

    Update index.php

--- week3_auth/index.php
+++ week3_auth/index.php
@@ -26,9 +26,43 @@ if (!isset($_SESSION['username'])):
 
 endif;
 
-    $username = $_SESSION['username'];
-    print($username);
-                                                                                                                                                                                                       
+    if (isset($_POST['username']) && isset($_POST['password'] && $_POST['username'] != "" && $_POST['password'] != "") {                                                                                                                 
+    // yes, check db  
+    require_once("passwords.php");
+    $mysqli = mysqli_connect($host, $user, $pass, $db);
+    if (mysqli_connect_errno($mysqli)) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        die;
+    }
+    // select password from user where user=$_POST['user'] -> do it ina prepare 
+    if ($stmt = $mysqli->prepare("select password from users where user=?")) {
+        print "failed to prepare " . $mysqli->error;
+        return;
+    }
+    if (!$stmt->bind_param("s",$username)) { 
+        print $mysqli->error;
+        return;
+    }
+    if (!$stmt->execute() {
+        print $mysqli->error;
+        return;
+    }
+    if (!$stmt->bind_result($storedPass)) {
+        print "Failed to bind output";
+        return;
+    }
+    $testpass = md5($_POST['password');
+    while ($stmt->fetch()) {
+        if ($testpass == $storedPass){
+            //good password -> set session  
+            $username = $_SESSION['username'];
+        }                                                                          
+        else {
+            $error = "invalid pass";
+        }
+    }
+}   
+                                                                                                                                                                                             
                                                                                                                  
 ?>
 
