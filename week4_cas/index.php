40535633a6ca7d9f628e278473c49b13b924c0c6
TaJah Reynolds <reynolt4@miamioh.edu>
2021-02-22 23:00:39 -0500

    Update index.php

--- week4_cas/index.php
+++ week4_cas/index.php
@@ -3,14 +3,25 @@
 session_start();
 
 require_once 'config.php';   //get configuration
-require_once 'CAS.php';      //load CAS
+require_once 'CAS.php';      //load CAS lib
 phpCAS::setDebug('/tmp/phpCAS'); //enable debugging
 
 // Initialize phpCAS from settings in config.php
 phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
-phpCAS::setNoCasServerValidation();  // disables ssl server verification - ok for testing and required for now
 
+// disables ssl server verification - ok for testing and required for now
+phpCAS::setNoCasServerValidation();  
+
+// For production use set the CA certificate that is the issuer of the cert
+// on the CAS server and uncomment the line below
+// phpCAS::setCasServerCACert($cas_server_ca_cert_path);
+
+// force CAS authentication
 phpCAS::forceAuthentication();
+
+// at this step, the user has been authenticated by the CAS server
+// and the user's login name can be read with phpCAS::getUser().
+
 $username = phpCAS::getUser();
 $_SESSION['username'] = $username;
 
