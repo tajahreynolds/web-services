fad8f36800acf025a3a5c090798b42fedefcec5d
TaJah Reynolds <reynolt4@miamioh.edu>
2021-03-03 14:08:30 -0500

    exam1 q1

---
+++ exam1.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+if (isset($_POST['hazel']))
+{
+if (hazel%2 == 0) 
+{
+print "even";
+} else
+{
+	for (int i = 0; i < hazel; i++) {
+		print "random";
+	}
+}
+}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>exam1-1</title>
+</head>
+<body>
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+Number: <input type="number" name="hazel"><br>
+<input type="submit">
+</form>
+</body>
+</html>
+
+?>
