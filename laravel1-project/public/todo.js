2720e7a628a0bc02e8ae4a483d64ac624e2056e6
TaJah Reynolds <reynolt4@miamioh.edu>
2021-03-15 12:25:08 -0400

    Create todo.js

---
+++ laravel1-project/public/todo.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+	$ajax({
+		url: 'api/todo'
+	}).done(function(data) {
+		for (i = 0; i < data.tasks.length; i++) {
+			$("#tasks").append("<li>" + data.tasks[i] + "</li>");
+		}
+	}).error(function(error) {
+		console.error(error);
+		$("#msg").html("Error retrieving data");
+	});
+}
