dee2e407f87b625886c4ec89be5d4ced3f96ead9
TaJah Reynolds <reynolt4@miamioh.edu>
2021-03-15 12:25:44 -0400

    add button to main layout

--- laravel1-project/resources/views/components/main-layout.blade.php
+++ laravel1-project/resources/views/components/main-layout.blade.php
@@ -53,6 +53,7 @@
 <a class='btn btn-info' href='{{ env('APP_URL')}}/index.php/about'>about</a>
 <a class='btn btn-info' href='{{ env('APP_URL')}}/index.php/room'>Room Listing</a>
 <a class='btn btn-info' href='{{ env('APP_URL')}}/index.php/room/add'>Add Room</a>
+<a class='btn btn-info' href='{{ env('APP_URL')}}/index.php/todo'>Todo List</a>
 </footer>
 
       </body>
